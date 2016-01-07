d25e07def1d345d87a14e17bf6f1a3941d8154d1
Scott Polhemus <scott.polhemus@barrelny.com>
2016-01-07 12:43:00 -0500

    add lib subfolders (fixing case-sensitive folder names)

---
+++ lib/Admin/AdminPage.php
@@ -0,0 +1,71 @@
+<?php
+namespace Barrel\SocialFeeds\Admin;
+
+/**
+ * Base admin page class, initializes common functionality for admin pages.
+ */
+class AdminPage {
+
+  static $page_title = 'Settings';
+  static $menu_title = 'Settings';
+  static $parent_slug = 'edit.php?post_type=social-post';
+  static $capability = 'manage_options';
+  static $menu_slug = 'settings';
+
+  function __construct() {
+    add_action('admin_init', array($this, 'initialize_options'));
+    add_action('admin_menu', array($this, 'add_options_page'));
+  }
+
+  /**
+   * Register any options for this admin page (called on admin_init).
+   */
+  function initialize_options() {
+    
+  }
+
+  /**
+   * Adds this page to the admin menu using static properties (can be overridden by child classes).
+   */
+  function add_options_page() {
+    add_submenu_page(
+      static::$parent_slug,
+      static::$page_title,
+      static::$menu_title,
+      static::$capability,
+      static::$menu_slug,
+      array($this, 'display_options_page')
+    );
+  }
+
+  /**
+   * Render the HTML for the admin page content.
+   */
+  function display_options_page() {
+    ?>
+    <div class="wrap">
+      <h2><?= static::$page_title ?></h2>
+      <form action="options.php" method="post">
+        <?php
+        do_settings_sections( static::$menu_slug );
+        submit_button();
+        ?>
+      </form>
+    </div>
+    <?php
+  }
+
+  /**
+   * Render the HTML for a single text field setting.
+   */
+  function render_text_setting($args) {
+    $option = get_option( $args[0] );
+    
+    echo '<input type="text" id="twitter" name="'.$args[0].'" value="' . htmlspecialchars($option) . '" size="50" />';
+    
+    if(isset($args[1])) {
+      echo '<br/><i>'.$args[1].'</i>';
+    }
+  }
+
+}
---
+++ lib/Cron/Update.php
@@ -0,0 +1,97 @@
+<?php
+namespace Barrel\SocialFeeds\Cron;
+use MetzWeb\Instagram\Instagram;
+
+/**
+ * Pulls the latest social posts and saves them to custom post type.
+ */
+class Update {
+
+  /**
+   * Initializes the Instagram API and fetches the latest posts
+   */
+  function __construct() {
+    $this->instagram = new Instagram(array(
+      'apiKey'      => get_option('instagram_client_id'),
+      'apiSecret'   => get_option('instagram_client_secret'),
+      'apiCallback' => get_bloginfo('url').'/instagram-callback/'
+    ));
+
+    $this->fetch_instagram();
+  }
+
+  /**
+   * Fetches Instagram posts based on the provided usernames and hashtags
+   */
+  function fetch_instagram() {
+    $hashtag_string = trim(get_option('instagram_feed_hashtag'));
+
+    if(!empty($hashtag_string)) {
+      $hashtag_string = str_replace('#', '', $hashtag_string);
+      $tags = explode(' ', $hashtag_string);
+
+      foreach ($tags as $tag) {
+        $feed = $this->instagram->getTagMedia($tag, 30);
+        
+        foreach ($feed->data as $social_post) {
+          $this->update_social_post($social_post);
+        }
+      }
+    }
+
+    $username_string = trim(get_option('instagram_feed_username'));
+
+    if(!empty($username_string)) {
+      $usernames = explode(' ', $username_string);
+
+      foreach ($usernames as $name) {
+        $users = $this->instagram->searchUser($name, 1);
+        $user_id = $users->data[0]->id;
+        $feed = $this->instagram->getUserMedia($user_id, 30);
+
+        foreach ($feed->data as $social_post) {
+          $this->update_social_post($social_post);
+        }
+      }
+    }
+
+  }
+
+  /**
+   * Handles a social API response by adding or updating posts in custom post type.
+   */
+  function update_social_post($social_post) {
+    $existing = get_posts(array(
+      'post_type' => 'social-post',
+      'post_status' => 'any',
+      'meta_key' => 'social_post_permalink',
+      'meta_value' => $social_post->link
+    ));
+
+    if(!empty($existing)) {
+      $id = $existing[0]->ID;
+    } else {
+      $id = wp_insert_post(array(
+        'post_type' => 'social-post',
+        'post_title' => $social_post->caption->text
+      ));
+
+      update_post_meta($id, 'social_post_username', $social_post->user->username);
+      update_post_meta($id, 'social_post_permalink', $social_post->link);
+      update_post_meta($id, 'social_post_image', $social_post->images->standard_resolution->url);
+      update_post_meta($id, 'social_post_created', $social_post->created_time);
+      update_post_meta($id, 'social_post_details', json_encode($social_post));
+
+      if(isset($social_post->videos)) {
+        update_post_meta($id, 'social_post_video', $social_post->videos->standard_resolution->url);
+      }
+
+      media_sideload_image($social_post->images->standard_resolution->url, $id);
+
+      $images = array_values(get_attached_media('image', $id));
+
+      set_post_thumbnail($id, $images[0]->ID);
+    }
+  }
+
+}
