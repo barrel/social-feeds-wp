0cd17168c874ff2315a516b32a623073573a88cc
ScottPolhemus <scott.polhemus@barrelny.com>
2016-01-10 20:48:18 -0500

    Query social posts after update on selection page

--- lib/Admin/SelectPosts.php
+++ lib/Admin/SelectPosts.php
@@ -18,22 +18,6 @@ class SelectPosts extends AdminPage {
     parent::__construct();
 
     add_action('admin_post_curate_social_feed', array($this, 'curate_feed'));
-    add_action('current_screen', array($this, 'query_social_posts'));
-  }
-
-  /**
-   * Query social posts for the admin page
-   */
-  function query_social_posts($current_screen) {
-    if($current_screen->id == 'social-post_page_select-posts') {
-      $this->query = new \WP_Query(array(
-        'post_type' => 'social-post',
-        'post_status' => 'any',
-        'posts_per_page' => -1,
-        'meta_key' => 'social_post_created',
-        'orderby' => 'meta_value'
-      ));
-    }
   }
 
   function add_options_page() {
@@ -45,6 +29,14 @@ class SelectPosts extends AdminPage {
 
   function display_options_page() {
     new Update();
+
+    $social_post_query = new \WP_Query(array(
+      'post_type' => 'social-post',
+      'post_status' => 'any',
+      'posts_per_page' => -1,
+      'meta_key' => 'social_post_created',
+      'orderby' => 'meta_value'
+    ));
     ?>
     <div class="wrap">
       <h2><?= self::$page_title ?></h2>
@@ -54,8 +46,8 @@ class SelectPosts extends AdminPage {
         <?php submit_button(); ?>
         <div class="social-post-cards">
           <?php
-            if($this->query->have_posts()) {
-              while ($this->query->have_posts()) { $this->query->the_post();
+            if($social_post_query->have_posts()) {
+              while ($social_post_query->have_posts()) { $social_post_query->the_post();
                 $item_id = get_the_ID();
                 $item_checked = (get_post_status($item_id) == 'publish') ? 'checked' : '';
                 $item_image = get_post_meta($item_id, 'social_post_image', true);
